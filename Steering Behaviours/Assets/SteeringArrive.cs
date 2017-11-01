dbc6643d7f993ed1c05964bb0aee2b8b69467a86
Marc de Pedro <marcdp2012@gmail.com>
2017-11-01 18:44:45 +0100

    Player slows down when reaches target

--- Steering Behaviours/Assets/SteeringArrive.cs
+++ Steering Behaviours/Assets/SteeringArrive.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SteeringArrive : MonoBehaviour {
 
     public float max_force;
+    public float slow_area_radius;
 
     private Move move;
 
@@ -23,11 +24,22 @@ public class SteeringArrive : MonoBehaviour {
     public void Steer(Vector3 target_position)
     {
         Vector3 desired_velocity = target_position - transform.position;
-        desired_velocity = desired_velocity.normalized * move.max_velocity;
+        float distance = desired_velocity.magnitude;
+
+        if (distance < slow_area_radius)
+            desired_velocity = desired_velocity.normalized * move.max_velocity * (distance / slow_area_radius);
+        else
+            desired_velocity = desired_velocity.normalized * move.max_velocity;
 
         Vector3 steering = desired_velocity - move.GetVelocity();
         steering = steering.normalized * max_force;
 
         move.AddSteeringForce(steering);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(move.target.transform.position, slow_area_radius);
     }
 }
