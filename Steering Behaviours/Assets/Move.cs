042404a0098e30358abeeb972d4ac23b1db89371
Marc de Pedro <marcdp2012@gmail.com>
2017-11-01 17:16:08 +0100

    Object follows a target w/ steering force

---
+++ Steering Behaviours/Assets/Move.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public GameObject target;
+    public float max_velocity;
+
+    private Vector3 velocity = Vector3.zero;
+    private float rotation = 0.0f;
+
+    public void AddSteeringForce(Vector3 force)
+    {
+        velocity += force;
+    }
+
+    public Vector3 GetVelocity()
+    {
+        return velocity;
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // Cap velocity
+		if (velocity.magnitude > max_velocity)
+        {
+            velocity = velocity.normalized * max_velocity;
+        }
+
+        // Move
+        transform.position += velocity * Time.deltaTime;
+    }
+}
---
+++ Steering Behaviours/Assets/SteeringArrive.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteeringArrive : MonoBehaviour {
+
+    public float max_force;
+
+    private Move move;
+
+	// Use this for initialization
+	void Start ()
+    {
+        move = GetComponent<Move>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Steer(move.target.transform.position);
+	}
+
+    public void Steer(Vector3 target_position)
+    {
+        Vector3 desired_velocity = target_position - transform.position;
+        desired_velocity = desired_velocity.normalized * move.max_velocity;
+
+        Vector3 steering = desired_velocity - move.GetVelocity();
+        steering = steering.normalized * max_force;
+
+        move.AddSteeringForce(steering);
+    }
+}
